00cbd634575d783e9de4a6ef97f602386cf2b1fe
jiw065 <wwjjjj0923@gmail.com>
2019-06-05 20:21:17 -0400

    update sql schema

--- spring-boot-crud-app/src/main/resources/static/schema.sql
+++ spring-boot-crud-app/src/main/resources/static/schema.sql
@@ -8,8 +8,16 @@ CONSTRAINT TB_AREA_PK PRIMARY KEY (AREA_ID)
 );
 
 CREATE SEQUENCE TB_AREA_SEQ START WITH 1 INCREMENT BY 1;
+ALTER SEQUENCE TB_AREA_SEQ INCREMENT BY 1;
+select TB_AREA_SEQ.Nextval from dual; 
 
 
+CREATE OR REPLACE TRIGGER BEF_INSERT_AREA 
+BEFORE INSERT ON TB_AREA
+FOR EACH ROW
+BEGIN
+  :NEW.AREA_ID := TB_AREA_SEQ.NEXTVAL; 
+END;  
 
 insert into tb_area(area_id,
                     area_name,
@@ -29,4 +37,25 @@ insert into tb_area(area_id,
                     area_name,
                     priority,
                     create_time)
-values(tb_area_seq.nextval,'Markham',3,sysdate);  
+values(tb_area_seq.nextval,'Markham',3,sysdate);    
+
+
+insert into tb_area(area_id,
+                    area_name,
+                    priority,
+                    create_time)
+values(0,'Richmond Hill',4,sysdate);  
+
+select area_id,
+       area_name,
+       priority,
+       create_time from tb_area;
+
+SELECT AREA_ID,
+       AREA_NAME,
+       PRIORITY,
+       CREATE_TIME FROM TB_AREA
+       WHERE AREA_ID = 1
+       
+       
+       
